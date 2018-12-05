03b856be43492a08aa050622006df75b485c2851
suro119 <sulee1415@gmail.com>
2018-12-05 22:55:56 +0900

    Update tutor.php

--- tutor.php
+++ tutor.php
@@ -17,11 +17,7 @@ if($count == 1) {
     header("location: mydbtutorlogin.php");
 }
 
-$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-$tid = $row['TutorID'];
 $sql = "select * from mydb.post where TutorID = $tid";
-
-
 $result = mysqli_query($db,$sql);
 echo "<table><tr><td>course</td><td>description</td><td>status</td></tr>";
 while ($data = mysqli_fetch_array($result)){
